5bcf14dc615887f7084a365233e9822658bf306a
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2016-10-06 10:35:29 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,12 @@
+class Join{
+	public static void main(String args[]){
+ 
+ 
+		int num = Integer.parseInt(args[0]);
+		String result = " ";
+		for(int i=1;i<=num;i++){
+			result = result + i + " ";
+		}
+		System.out.println(result);
+	}
+}
