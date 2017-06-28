ed68c86f56d747a535945a1f7cf5d083a704c855
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2017-06-28 12:42:03 +0530

    Create robot.c

---
+++ robot.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+#include<coino.h>
+void main()
+{
+int i,m,x,y,c,d;
+printf("A robot have 2 type of commands\n");
+scanf("%d",&m);
+for(i=0;i<=x;i++)
+{
+if(x>=0 && x<=10000)
+{
+ printf("c1: forward%d",x);
+}
+}
+for(i=0;i<=y;i++)
+{
+if(y==-180 && y==180)
+{
+printf("c2: X degree:%d",y);
+}
+}
+
+}
