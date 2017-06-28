598553554823e7c2e497de6c6c01b2c426aa4846
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2017-06-28 12:17:32 +0530

    Create gard.c

---
+++ gard.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,n,i,j,count=0;
+scanf("%d",&a);
+n=2*a;
+for(i=1;i<n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+printf("(%d,%d)\n",i,j);
+count++;
+}
+}
+printf("No of paires =%d",count);
+getch();
+return 0;
+}
