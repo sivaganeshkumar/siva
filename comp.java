72b29e7b65678c3c6b149599f1a45a307adf46e2
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2017-06-28 16:00:24 +0530

    Create comp.java

---
+++ comp.java
@@ -0,0 +1,19 @@
+package com.myjava.string;
+ 
+public class MyStrMatches
+{
+ 
+    public static void main(String a[])
+    {
+     
+        String[] str = {"www.java2novice.com", "http://java2novice.com"};
+        for(int i=0;i < str.length;i++){
+            if(str[i].matches("^www\\.(.+)")){
+                System.out.println(str[i]+" Starts with 'www'");
+            }
+            else 
+            {
+                System.out.println(str[i]+" is not starts with 'www'");
+            }
+        }
+}
