f97275a736aa4dad92b21255554de417c9da3c4c
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2017-06-28 12:40:35 +0530

    Create rect.c

---
+++ rect.c
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+struct Point
+{
+	int x, y;
+};
+
+bool doOverlap(Point l1, Point r1, Point l2, Point r2)
+{
+
+	if (l1.x > r2.x || l2.x > r1.x)
+		return false;
+
+	if (l1.y < r2.y || l2.y < r1.y)
+		return false;
+
+	return true;
+}
+
+void main()
+{
+	Point l1 = {0, 10}, r1 = {10, 0};
+	Point l2 = {5, 5}, r2 = {15, 0};
+	if (doOverlap(l1, r1, l2, r2))
+		printf("Rectangles Overlap");
+	else
+		printf("Rectangles Don't Overlap");
+
+}
