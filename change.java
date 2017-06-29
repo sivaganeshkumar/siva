6d4f08d2cf68179a14200b82fc2a00014e9e07ba
sivaganeshkumar <sivaganeshkumar.d@gmail.com>
2017-06-29 11:19:28 +0530

    Create change.java

---
+++ change.java
@@ -0,0 +1,30 @@
+package odd;
+
+
+public class Change
+{
+   public static int strToInt( String str )
+   {
+        
+    int i = 0;
+    int num = 0;
+    boolean isNeg = false;
+
+   
+    if (str.charAt(0) == '-') 
+    {
+        isNeg = true;
+        i = 1;
+    }
+
+    
+    while( i < str.length())
+    {
+        num *= 10;
+        num += str.charAt(i++) - '0'; 
+
+    if (isNeg)
+        num = -num;
+    return num;
+}
+}
